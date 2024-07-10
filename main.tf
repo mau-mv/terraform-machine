5d6a21aba39297a58d8dc07996d43a7294317dfd
mau-mv <morm97@hotmail.com>
2024-07-10 13:01:48 -0600

    another trigger test

--- main.tf
+++ main.tf
@@ -50,9 +50,9 @@ resource "aws_subnet" "secondary" {
   }
 }
 
-resource "aws_subnet" "trigger_test"{
+resource "aws_subnet" "trigger_test2"{
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.4.0/24"
+  cidr_block = "10.0.5.0/24"
   availability_zone = "us-east-1b"
 }
 
